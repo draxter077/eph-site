f807b7c06061379d749e18442ceb04a4ec8e1f63
philippe <philippe.idalgoprestes@gmail.com>
2025-04-27 11:38:08 -0300

    seen & title

--- pages/main/section/items/item/seen/main.js
+++ pages/main/section/items/item/seen/main.js
@@ -4,11 +4,10 @@ export default function seen(){
             width:100%;
             text-align:center;
             color:var(--colorOrange);
-            font-size:calc(0.75*var(--productPriceFontSize));
-            padding:1px;
+            font-size:calc(0.8*var(--productPriceFontSize));
             border-radius:0px 10px 10px 0px;
         }
-        :responsive{font-size:calc(0.6*var(--productPriceFontSize))}`
+        :responsive{font-size:calc(0.7*var(--productPriceFontSize))}`
 
     const seen = cE("div", style)
     let views = Math.floor(10*(Math.random()))
