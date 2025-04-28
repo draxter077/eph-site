6336f1b22c4fde5426d30ccbe327dd9875689fff
philippe <philippe.idalgoprestes@gmail.com>
2025-04-27 21:34:52 -0300

    better bot text animation

--- pages/main/disclaimer/text/main.js
+++ pages/main/disclaimer/text/main.js
@@ -14,25 +14,32 @@ export default function text(){
             border-radius:10px 10px 0px 10px;
             margin:10px 0px 0px 0px;
         }
-        :responsive{font-size:calc(0.5*var(--titleFontSize))}`
+        :responsive{
+            font-size:calc(0.5*var(--titleFontSize));
+        }`
 
     const text = cE("div", style)
     let t = "Olá! Sou o Eph, seu assistente de compras online! Vasculho os maiores marketplaces 24h por dia para trazer as melhores ofertas com os melhores preços! Minha inteligência artificial seleciona promoções imperdíveis em todas as categorias, atualizando nossas opções diariamente. Aqui você encontra todos os meus achados! Aproveite!"
+
     for(let i = 0; i < t.split(" ").length; i++){
         let ti = t.split(" ")[i]
         if(i > 0 && ti != "."){text.appendChild(word("⠀"))}
         text.appendChild(word(ti))
     }
+
     window.addEventListener(
         "load",
         async function a(){
             await new Promise(resolve => setTimeout(resolve, 3000))
             let previousChar = ""
+
             for(let i = 0; i < text.children.length; i++){
                 if(i > 0){text.children[i - 1].innerHTML = previousChar}
                 text.children[i].style.opacity = 1
                 previousChar = text.children[i].innerHTML
-                text.children[i].innerHTML = "_"
+
+                for(let k = 0; k < previousChar.length; k++){text.children[i].innerHTML += "_"}
+
                 if(i == text.children.length - 1){text.children[i].innerHTML = previousChar}
                 await new Promise(resolve => setTimeout(resolve, 50))
             }
