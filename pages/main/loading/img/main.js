bcbd7eed7acab80a469bccb450547a4f31c7d9b5
philippe <philippe.idalgoprestes@gmail.com>
2025-05-01 10:05:41 -0300

    loading

--- pages/main/loading/img/main.js
+++ pages/main/loading/img/main.js
@@ -2,14 +2,15 @@ export default function img(){
     let style = `
         {
             width:50%;
+            opacity:0;
             animation:alive calc(3*var(--transitionTime)) linear 0s infinite alternate;
         }
         @keyframes alive{
-            0%[transform:scale(1)]
-            100%[transform:scale(1.1)]
+            0%[opacity:0]
+            100%[opacity:1]
         }`
 
     const img = cE("img", style)
-    img.src = "./assets/logo.png"
+    img.src = "./assets/counterlogo.png"
     return(img)
 }
