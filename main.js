583aa8872d0d28eeb2fdff4593b60b5f8c31097c
philippe <philippe.idalgoprestes@gmail.com>
2025-05-08 01:08:17 -0300

    url still

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ window.construct = function construct(p){
             //    root.appendChild(feed())
             //}
             if(paths[0].split("?")[0] == "categoria"){
-                root.appendChild(category(paths[0].split("?")[1]))
+                root.appendChild(category(paths[0].split("?")[1].replaceAll("%20", " ").replaceAll("%C3%B4", "ô").replaceAll("%C3%A7", "ç").replaceAll("%C3%B3", "ó"),replaceAll("%C3%AA", "ê").replaceAll("%C3%93", "Ó")))
             }
             else{
                 root.appendChild(main())
