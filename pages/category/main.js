ffca90d38e027382925cff5add1dcdcb7648cf9f
philippe <philippe.idalgoprestes@gmail.com>
2025-05-08 01:10:00 -0300

    mmm

--- pages/category/main.js
+++ pages/category/main.js
@@ -7,6 +7,7 @@ import foot from "../common/foot/main.js"
 import products from "../../products.js"
 
 export default function category(c){
+    console.log(c)
     let style = `
         {
             display:flex;
