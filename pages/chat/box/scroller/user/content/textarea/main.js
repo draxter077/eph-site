d48217cc0e04222fe69adac312d5005affda5a12
philippe <philippe.idalgoprestes@gmail.com>
2025-07-31 16:29:21 -0300

    prompt

--- pages/chat/box/scroller/user/content/textarea/main.js
+++ pages/chat/box/scroller/user/content/textarea/main.js
@@ -31,7 +31,7 @@ export default function textarea(){
 
                 let ids = []
                 axios.defaults.headers.common["X-goog-api-key"] = "AIzaSyD7Gqevs5TflzqC87AFi6pMEXhpNXueKUg" // Faz o pedido para o Google
-                let req = `Considerando esta lista de produtos ${JSON.stringify(products)}, retorne o maior número de IDs que satisfaçam o seguinte pedido ${e.target.value}. Sua resposta deve ser no formato "ID,ID,ID", sem nenhum outro conteúdo nela.`
+                let req = `Considerando esta lista de produtos ${JSON.stringify(products)}, retorne o maior número de IDs que satisfaçam o seguinte pedido "${e.target.value}". Sua resposta deve ser no formato "ID,ID,ID", sem nenhum outro conteúdo nela.`
                 await axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent", {"contents": [{"parts": [{"text":req}]}]})
                     .then(r =>  ids = r.data.candidates[0].content.parts[0].text.split(","))
                     .catch(r => console.log(r))
